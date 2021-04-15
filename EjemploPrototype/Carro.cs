754a2539a8c6f9b511ecea1362cd24cab4cfefbe
CIUP _Juan Esteban Ramos <JRP-CIUP@enka.com.co>
2021-04-14 19:54:03 -0500

    Agregar archivos de proyecto.

---
+++ EjemploPrototype/Carro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploPrototype
+{
+    public class Carro :ICloneable
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public object Clone()
+        {
+           return this.MemberwiseClone();
+        }
+    }
+}
---
+++ EjemploPrototype/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EjemploPrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro() { Marca = "Ferrari", Modelo = "Spider" };
+            Carro carroClonado = carro.Clone() as Carro;
+            carroClonado.Modelo = "Monza";
+
+            Console.WriteLine("*********OBJETO ORIGINAL**********");
+            Console.WriteLine("Marca: " + carro.Marca + " Modelo: " + carro.Modelo + "\n");
+            Console.WriteLine("*********OBJETO CLONADO**********");
+            Console.WriteLine("Marca: " + carroClonado.Marca + " Modelo: " + carroClonado.Modelo + "\n");
+            Console.ReadKey();
+        }
+    }
+}
